15f228a9da2f6441648bd5cb4dd0a85aea494853
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2025-05-02 15:25:09 +0600

    Added off plan single page

--- src/Routers/Routes.jsx
+++ src/Routers/Routes.jsx
@@ -10,51 +10,56 @@ import Contact from "../components/Contact/Contact";
 import TeamPage from "../components/TeamMembers/TeamPage";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import OffPlanListingPage from "../pages/OffPlanProperties/OffPlanPropertyListingPage";
+import OffPlanSinglePage from "../pages/OffPlanSingle/OffPlanSingle/OffPlanSInglePage";
 
 const router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
         path: "/",
-        element: <Layout></Layout>,
-        errorElement: <ErrorPage></ErrorPage>,
-        children: [
-            {
-                path: "/",
-                element: <Home></Home>
-            },
-            {
-                path: "/buy",
-                element: <Buy></Buy>
-            },
-            {
-                path: "/rent",
-                element: <Rent></Rent>
-            },
-            {
-                path: "/property-details",
-                element: <PropertyDetails></PropertyDetails>
-            },
-            {
-                path: "/off-plan-properties",
-                element: <OffPlanListingPage></OffPlanListingPage>
-               },
-           {
-            path: "/off-plan-property",
-            element: <OffPlanProperty></OffPlanProperty>
-           },
-           {
-            path: "/contact",
-            element: <Contact></Contact>
-           },
-           {
-            path: "/agent",
-            element: <AgentProfileCard></AgentProfileCard>
-           },
-           {
-            path: "/our-team",
-            element: <TeamPage></TeamPage>
-           }
-        ]
-    }
-])
+        element: <Home></Home>,
+      },
+      {
+        path: "/buy",
+        element: <Buy></Buy>,
+      },
+      {
+        path: "/rent",
+        element: <Rent></Rent>,
+      },
+      {
+        path: "/property-details",
+        element: <PropertyDetails></PropertyDetails>,
+      },
+      {
+        path: "/off-plan-properties",
+        element: <OffPlanListingPage></OffPlanListingPage>,
+      },
+      {
+        path: "/off-plan-property",
+        element: <OffPlanProperty></OffPlanProperty>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/agent",
+        element: <AgentProfileCard></AgentProfileCard>,
+      },
+      {
+        path: "/our-team",
+        element: <TeamPage></TeamPage>,
+      },
+      {
+        path: "/off-plan-single",
+        element: <OffPlanSinglePage></OffPlanSinglePage>,
+      },
+    ],
+  },
+]);
 
 export default router;
